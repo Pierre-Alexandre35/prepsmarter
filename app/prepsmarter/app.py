130e34324a13bdfaaf48dad89077c93410c28ee0
Pierre <mousset.p@husky.neu.edu>
2021-02-17 22:10:32 +0100

    (major) added blueprints / added static files / added Dockerfile / added virtualenv

---
+++ app/prepsmarter/app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+
+from prepsmarter.blueprints.page import page
+from prepsmarter.blueprints.admin import admin
+
+def create_app(settings_override=None):
+    """
+    Create a Flask application using the app factory pattern.
+
+    :param settings_override: Override settings
+    :return: Flask app
+    """
+    app = Flask(__name__, instance_relative_config=True)
+
+    app.config.from_object('config.settings')
+    app.config.from_pyfile('settings.py', silent=True)
+
+    if settings_override:
+        app.config.update(settings_override)
+
+    app.register_blueprint(page)
+    app.register_blueprint(admin)
+
+    return app
