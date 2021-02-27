6274abb58bb85a9046fff4b8ce0b2db7a8681ef7
Pierre <mousset.p@husky.neu.edu>
2021-02-27 10:20:09 +0100

    (update) removed pycache stuff

---
+++ app/prepsmarter/blueprints/user/models.py
@@ -0,0 +1,16 @@
+class User():
+    id = 1
+    email = "pamousset75@gmail.com"
+    password = "bonjour"
+    registration_date = '20/07/2020'
+    is_active = True
+    
+    
+     # Activity tracking.
+    sign_in_count = 5433
+    current_sign_in_on = 'eeee'
+    current_sign_in_ip = 'eeee'
+    last_sign_in_on = 'fff'
+    last_sign_in_ip = 'fddfdf'
+    
+    
--- app/prepsmarter/blueprints/user/views.py
+++ app/prepsmarter/blueprints/user/views.py
@@ -4,6 +4,11 @@ from prepsmarter.extensions import conn
 user = Blueprint('user', __name__, template_folder='templates')
 
 
-@user.route('/login')
+@user.route('/register')
 def login():
-    return render_template('login.html')
+    return render_template('register.html')
+
+
+@user.route('/new-user',methods = ['POST'])
+def new_user():
+    return "dd"
