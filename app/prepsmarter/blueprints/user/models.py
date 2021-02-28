21843b135a904184e43276d5957c14fea9749a8e
Pierre <mousset.p@husky.neu.edu>
2021-02-28 12:12:42 +0100

    (update) added methods on the UserServices and only keep getters and seeters on the User class

--- app/prepsmarter/blueprints/user/models.py
+++ app/prepsmarter/blueprints/user/models.py
@@ -1,8 +1,44 @@
 import datetime
 from hashers import get_hashed_password
 
+
+class UserService():
+    def register_user(self,
+                      email,
+                      password,
+                      registration_date,
+                      active,
+                      sign_in_count,
+                      current_sign_in_on,
+                      last_sign_in_on):
+        new_user = User(email, password, registration_date, active,
+                        sign_in_count, current_sign_in_on, last_sign_in_on)
+        return new_user
+
+    def desactivate_user(self, User):
+        if User.active == False:
+            print(f"User {User.email} is already inactive")
+        User.active = False
+
+    def reactive_user(self, User):
+        if User.active == True:
+            print(f"User {User.email} is already active")
+        User.active = True
+
+    def is_active(self, User):
+        return User.is_active
+
+    def update_activity_tracking(self, User, ip_address):
+        User.sign_in_count += 1
+        User.last_sign_in_on = User.current_sign_in_on
+        User.current_sign_in_on = datetime.datetime.now()
+
+    def update_password(self, User, new_password):
+        User.password = get_hashed_password(new_password)
+
+
 class User():
-    def __init__(self, email, password, registration_date, active, sign_in_count, current_sign_in_on, current_sign_in_ip, last_sign_in_on, last_sign_in_ip):
+    def __init__(self, email, password, registration_date, active, sign_in_count, current_sign_in_on, last_sign_in_on):
         self.email = email
         self.password = password
         self.registration_date = registration_date
@@ -11,54 +47,23 @@ class User():
         # Activity tracking
         self.sign_in_count = sign_in_count
         self.current_sign_in_on = current_sign_in_on
-        self.current_sign_in_ip = current_sign_in_ip
         self.last_sign_in_on = last_sign_in_on
-        self.last_sign_in_ip = last_sign_in_ip
-
-    def desactivate_user(self):
-        if self.active == False:
-            print(f"User {self.email} is already inactive")
-        self.active = False
-
-    def reactive_user(self):
-        if self.active == True:
-            print(f"User {self.email} is already active")
-        self.active = True
-
-    def is_active(self):
-        return self.active
-
-    def update_activity_tracking(self, ip_address):
-        self.sign_in_count += 1
-        self.last_sign_in_on = self.current_sign_in_on
-        self.last_sign_in_ip = self.current_sign_in_ip
-        self.current_sign_in_on = datetime.datetime.now()
-        self.current_sign_in_ip = ip_address
-
-
-    def update_password(self, new_password):
-        self.password = get_hashed_password(new_password)
 
     def __str__(self):
         user_attributes = vars(self)
         return (', '.join("%s: %s" % item for item in user_attributes.items()))
-    
-    def save(self):
-        # commit query should be in another class
-        return True
 
-pierre = User('pamousset', 
-              'Bonjour', 
-              '2021-02-13', 
-              False,
-              3,
-              '2022-10-10',
-              '192.1.1',
-              '2010-12-10',
-              '192.1.1')
 
-print('azerty'.encode('utf-8'))
+user_service = UserService()
 
-pierre.update_password('azerty'.encode('utf-8'))
-print(pierre.password)
+user_1 = user_service.register_user('pamousset01@gmail.com',
+                                     '200795jtm1',
+                                     '2020-01-12',
+                                     True,
+                                     13,
+                                     '2021-02-21',
+                                     '2021-02-20')
 
+user_service.desactivate_user(user_1)
+
+print(user_1.__str__())
